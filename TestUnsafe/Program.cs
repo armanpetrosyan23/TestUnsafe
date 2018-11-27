9eeeefcd17fc29a793466b0327f5a1d5da4bd5d0
Arman Petrosyan <armanpetrosyan181@gmail.com>
2018-11-27 19:06:29 +0400

    Added

---
+++ TestUnsafe/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestUnsafe
+{
+    class Program
+    {
+        static unsafe void Main(string[] args)
+        {
+            int p1 = 32000000;
+
+            int* pointer1 = (int*)&p1;
+
+            //This is address in memory 
+            Console.WriteLine((long)(pointer1));
+            for (int i = 0; i < 4; i++)
+            {
+                //This is address in memory after add one bytes 
+                Console.WriteLine((int)((byte*)pointer1 + i));
+
+                Console.WriteLine((*((byte*)pointer1 + i)));
+
+            }
+            /* Output
+            9434224           
+            9434224
+            255
+            9434225
+            255
+            9434226
+            255
+            9434227
+            127
+              */
+
+        }
+                
+    }
+}
