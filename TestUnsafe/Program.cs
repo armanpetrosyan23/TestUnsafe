b6018f93f56bc3f2e08f9be3af68d22f366d2d3d
Arman Petrosyan <armanpetrosyan181@gmail.com>
2018-11-27 21:36:32 +0400

    Added two exapmle

--- TestUnsafe/Program.cs
+++ TestUnsafe/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,71 @@ namespace TestUnsafe
     class Program
     {
         static unsafe void Main(string[] args)
+        {
+            int length = (int)10E7;
+            Stopwatch timer = new Stopwatch();
+            int[] test = new int[length];
+
+            fixed (int* p = &test[0])
+            {
+                timer.Start();
+
+                for (int i = 0; i < length; i++)
+                {
+                    *(p + i) = i;
+                    int value = (int)*(p + i);
+
+                }
+                timer.Stop();
+                Console.WriteLine(timer.ElapsedMilliseconds);
+            }
+
+            int[] test1 = new int[length];
+            timer.Start();
+            for (int i = 0; i < length; i++)
+            {
+                test1[i] = i;
+                int value = test[i];
+            }
+            timer.Stop();
+            Console.WriteLine(timer.ElapsedMilliseconds);
+          
+            // pointer result twice faster
+
+        }
+
+        static unsafe void ArrayInStack()
+        {
+
+            int length = (int)10E5;
+            Stopwatch timer = new Stopwatch();
+
+            int* pArray = stackalloc int[length];
+            timer.Start();
+
+            for (int i = 0; i < length; i++)
+            {
+                Math.Pow((int)*(pArray + i), i);
+
+            }
+
+            timer.Stop();
+            Console.WriteLine(timer.ElapsedMilliseconds);
+
+            int[] array = new int[length];
+            timer.Start();
+            for (int i = 0; i < length; i++)
+            {
+                Math.Pow(array[i], i);
+            }
+            timer.Stop();
+            Console.WriteLine(timer.ElapsedMilliseconds);
+
+            // pointer result twice faster
+        }
+
+
+        static unsafe void MemoryLocation()
         {
             int p1 = 32000000;
 
@@ -20,23 +86,14 @@ namespace TestUnsafe
             {
                 //This is address in memory after add one bytes 
                 Console.WriteLine((int)((byte*)pointer1 + i));
-
                 Console.WriteLine((*((byte*)pointer1 + i)));
 
             }
-            /* Output
-            9434224           
-            9434224
-            255
-            9434225
-            255
-            9434226
-            255
-            9434227
-            127
-              */
+
 
         }
-                
+
     }
 }
+
+
